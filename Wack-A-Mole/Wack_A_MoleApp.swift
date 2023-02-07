70de316a5dc38b821b65ac3039f8a07abd97f5ab
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-02-07 21:38:14 +0800

    Initial Commit

---
+++ Wack-A-Mole/Wack_A_MoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wack_A_MoleApp.swift
+//  Wack-A-Mole
+//
+//  Created by Jia Chen Yee on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Wack_A_MoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
