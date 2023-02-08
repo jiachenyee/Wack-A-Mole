fedab0e6e9685e6d83fa5e10e64cc6efb35ee9ba
Du Yuhan <yuhan.du@gmail.com>
2023-02-08 20:07:04 +0800

    Add whack-a-mole popping functionality

--- Wack-A-Mole/GameView.swift
+++ Wack-A-Mole/GameView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 struct GameView: View {
     
     @ObservedObject var manager: Manager
+    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    @State var generatedNumber = Int.random(in: 1...20)
+
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if generatedNumber == 12 {
+                Button {
+                    manager.tapped()
+                } label: {
+                    Color(.white)
+                        .ignoresSafeArea()
+                }
+            } else {
+                Color(.black)
+                    .ignoresSafeArea()
+            }
+        }
+        .onReceive(timer) { _ in
+            let newNumber = Int.random(in: 1...20)
+            generatedNumber = newNumber
+            
+        }
     }
 }
